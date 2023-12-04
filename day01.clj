6accfc0883c33ced9f2bf5f574a2f77ddd28caf7
Vincent Garcia <vigarcia@semtech.com>
2023-12-04 16:23:57 +0100

    Rm mess

--- day01.clj
+++ day01.clj
@@ -29,35 +29,11 @@
           "eight" "8"
           "nine" "9"})
 
-
+(def d' (update-keys d str/reverse))
 
 (def answer2 (->> input
-                  (map #(clojure.string/replace % (re-pattern (str/join "|" (keys d))) d))
                   (map #(Integer/parseInt
                          (str
-                          (re-find #"\d" %)
-                          (re-find #"\d" (apply str (reverse %))))))
-                  #_(apply +)))
-
-;; (spit "a2.edn" (with-out-str (clojure.pprint/pprint answer2)))
-
-;; (clojure.string/replace "abcone2threexyz" (re-pattern (str/join "|" (keys d))) d)
-
-;; ;54473
-
-;; (clojure.string/replace "mnbrf3fourfpbrdgltf2xbmbmrbjltdxbklsixoneightq" (re-pattern (str/join "|" (keys d))) d)
-
-;; (re-pattern (re-pattern (str/join "|" (into (keys d) (vals d)))))
-;; "mnbrf3fourfpbrdgltf2xbmbmrbjltdxbklsixoneightq"
-
-;; (def % "mnfourbrf3fourfpbrdgltf2xbmbmrbjltdxbklsixoneightq")
-;; (identity
-;;  (str
-;;   (as-> (re-find (re-pattern (str/join "|" (into (keys d) (vals d)))) %) % (get d % %))
-;;   (let [e 5] (as-> (re-find (re-pattern (str/join "|" (into (keys d) (vals d)))) %) % (get d % %)))))
-
-;; (let [e 5] e)
-;; (Integer/parseInt
-;;  (str
-;;   (re-find #"\d" %)
-;;   (re-find #"\d" (apply str (reverse %)))))
+                          (let [r (re-find (re-pattern (str/join "|" (into (keys d) (vals d)))) %)] (get d r r))
+                          (let [r (re-find (re-pattern (str/join "|" (into (keys d') (vals d')))) (str/reverse %))] (get d' r r)))))
+                  (apply +)))
